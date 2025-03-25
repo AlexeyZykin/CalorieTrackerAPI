8fcc1207d9c908004570725b38cc8656852f29bb
Alexey <Alex.Zykin15@gmail.com>
2025-03-25 17:33:40 +0700

    Initial commit

---
+++ src/test/java/com/alexey/calorietrackerapi/CalorieTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexey.calorietrackerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalorieTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
